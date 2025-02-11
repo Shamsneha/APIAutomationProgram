6077d65f75426c99896269665de4de90b8420da8
Neha <Shamsneha48@gmail.com>
2025-02-11 20:03:55 +0530

    Changes APITest

--- src/test/java/com/thetestingacademy/APITesting_testngExample/Testng_Example014.java
+++ src/test/java/com/thetestingacademy/APITesting_testngExample/Testng_Example014.java
@@ -2,36 +2,39 @@ package com.thetestingacademy.APITesting_testngExample;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import sun.font.TrueTypeFont;
+
 
 public class Testng_Example014 {
-    @Test(groups = )
-    public void sanityRun(){
-        System.out.println("sanity");
-        System.out.println("QA");
-        Assert.assertTrue(true);
-
-    }
-    @Test
-    public void RegRun(){
-        System.out.println("Reg");
-        Assert.assertTrue(false);
-    }
-    @Test
-    public void SmokeRun(){
-        System.out.println("Smoke");
-        Assert.assertTrue(true);
-    }
-    @Test
-    public void SanityRun1(){
-        System.out.println("Sanity");
-        System.out.println("Sanity");
-        Assert.assertTrue(true);
-    }
-    @Test
-    public void SmokeRun2(){
-        System.out.println("Smoke");
-        Assert.assertTrue(false);
-    }
-
+        @Test(groups = {"sanity", "qa", "preprod"}, priority = 1)
+        public void sanityRun(){
+            System.out.println("Sanity");
+            System.out.println("QA");
+            Assert.assertTrue(true);
+        }
+        @Test(groups = {"qa","preprod", "reg"})
+        public void RegRun(){
+            System.out.println("Reg");
+            Assert.assertTrue(false);
+        }
+        @Test(groups = {"dev","stage"})
+        public void SmokeRun(){
+            System.out.println("Smoke");
+            Assert.assertTrue(false);
+        }
+        @Test(groups = {"sanity", "qa", "preprod"})
+        public void sanityRun1(){
+            System.out.println("Sanity");
+            System.out.println("QA");
+            Assert.assertTrue(true);
+        }
+        @Test(groups = {"qa","preprod", "reg"})
+        public void RegRun2(){
+            System.out.println("Reg");
+            Assert.assertTrue(false);
+        }
+        @Test(groups = {"dev","stage"})
+        public void SmokeRun3(){
+            System.out.println("Smoke");
+            Assert.assertTrue(false);
+        }
 }
--- src/test/java/com/thetestingacademy/Integration/APITesting011_TCIntegration.java
+++ src/test/java/com/thetestingacademy/Integration/APITesting011_TCIntegration.java
@@ -9,11 +9,12 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class APITesting011_TCIntegration {
+    //Create Token
     //Create a booking
     //Perform a put request
-    //Verify that Put Request is success by Get request
+    //Verify that Put Request is success by GET request
     //Delete the ID
-    //verify it dosn't exist Get request
+    //verify it doesn't exist Get request
     RequestSpecification requestSpecification;
     Response responce;
     ValidatableResponse validatableResponse;
@@ -41,30 +42,94 @@ public class APITesting011_TCIntegration {
     }
 
     public String getBookingID() {
-return null;
+
+        String payload_POST="\n" +
+                "    \"firstname\" : \"Jim\",\n" +
+                "    \"lastname\" : \"Brown\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : true,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2018-01-01\",\n" +
+                "        \"checkout\" : \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Breakfast\"\n" +
+                "}";
+        requestSpecification= RestAssured.given();
+        requestSpecification.baseUri("https://restful-booker.herokuapp.com/");
+        requestSpecification.basePath("/booking");
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.body(payload_POST).log().all();
+        Response response = requestSpecification.when().post();
+        ValidatableResponse validatableResponse = responce.then().log().all();
+
+        validatableResponse.statusCode(200);
+        //Extract the token
+        booking_id = responce.jsonPath().getString("bookingid");
+        System.out.println(booking_id);
+        return booking_id;
+
     }
-        @Test
+        @Test(priority = 1)
         public void test_update_request_put(){
-        String token = getToken();
+        token = getToken();
+        booking_id=getBookingID();
             System.out.println(token);
+            System.out.println(booking_id);
+            String payload_PUT="{\n" +
+                    "    \"firstname\" : \"Neha\",\n" +
+                    "    \"lastname\" : \"Shams\",\n" +
+                    "    \"totalprice\" : 111,\n" +
+                    "    \"depositpaid\" : true,\n" +
+                    "    \"bookingdates\" : {\n" +
+                    "        \"checkin\" : \"2018-01-01\",\n" +
+                    "        \"checkout\" : \"2019-01-01\"\n" +
+                    "    },\n" +
+                    "    \"additionalneeds\" : \"Breakfast\"\n" +
+                    "}";
+
+            requestSpecification = RestAssured.given();
+            requestSpecification.baseUri("https://restful-booker.herokuapp.com/");
+            requestSpecification.basePath("/booking/"+booking_id);
+            requestSpecification.contentType(ContentType.JSON);
+            requestSpecification.cookie("token",token);
+            requestSpecification.body(payload_PUT).log().all();
+            Response response = requestSpecification.when().put();
+            validatableResponse = response.then().log().all();
+            validatableResponse.statusCode(200);
 
         }
 
-        @Test
-        public void test_update_request_get () {
-
-
+        @Test(priority = 2)
+        public void test_update_request_get() {
+            System.out.println(booking_id);
+            requestSpecification.baseUri("https://restful-booker.herokuapp.com/");
+            requestSpecification.basePath("/booking/"+booking_id);
+            Response response = requestSpecification.when().log().all().get();
+            requestSpecification.then().log().all().statusCode(200);
+            String firstname = response.jsonPath().getString("firstname");
+            Assert.assertEquals(firstname,"Anshul");
 
         }
 
-            @Test
-            public void test_delete_booking () {
+            @Test(priority = 3)
+            public void test_delete_booking() {
                 System.out.println(booking_id);
                 System.out.println(token);
+                requestSpecification = RestAssured.given();
+                requestSpecification.baseUri("https://restful-booker.herokuapp.com/");
+                requestSpecification.basePath("/booking/"+booking_id);
+                requestSpecification.contentType(ContentType.JSON);
+                requestSpecification.cookie("token",token);
+                Response response = requestSpecification.when().delete();
+                validatableResponse = response.then().log().all();
+                validatableResponse.statusCode(201); // #TODO #1 -Delete Bug
             }
-            @Test
-            public void test_after_delete_request_get () {
-                System.out.println(booking_id);
+    @Test(priority = 4)
+    public void test_after_delete_request_get() {
+        requestSpecification.baseUri("https://restful-booker.herokuapp.com/");
+        requestSpecification.basePath("/booking/"+booking_id);
+        Response response = requestSpecification.when().log().all().get();
+        requestSpecification.then().log().all().statusCode(404);
+    }
 
-            }
-        }
+}
